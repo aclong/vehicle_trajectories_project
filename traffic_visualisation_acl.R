fde46083af74f2096a9b136c227fbd8b5eb1b9d6
aclong <37698923+aclong@users.noreply.github.com>
2018-10-24 13:00:25 +0100

    made some changes needs more work

--- traffic_visualisation_acl.R
+++ traffic_visualisation_acl.R
@@ -24,7 +24,7 @@ individ_vehicles <- read.csv(paste0(data_folder, "NGSIM_Peachtree_Vehicle_Trajec
 
 #now get some packages that you want
 library(raster)
-
+library(dplyr)
 
 #load in as raster
 atlanta_peach_raster <- raster(paste0(data_folder, "Atlanta-Peachtree.tif"))
@@ -39,3 +39,28 @@ plotRGB(atlanta_peach_brick)
 
 crs(atlanta_peach_brick)
 
+#find out the column names for aggregating the data
+
+colnames(individ_vehicles)
+
+lapply(individ_vehicles, class)
+
+#add cumulative lane change counter for vehicles
+individ_vehicles$cumu_lan_change <- for(i in individ_vehicles$Vehicle_ID){
+  for j in individ_vehicles$Frame_ID[i]{
+    if individ_vehicles$Lane_ID[j] != individ_vehicles$Lane_ID[j-1]
+    
+  }
+}
+
+vehicle_agg_df <- individ_vehicles %>%
+  group_by(Vehicle_ID) %>%
+  arrange(Frame_ID) %>%
+  summarise(average_speed = sum(v_Vel)/n(), 
+            sum_accel = sum(abs(v_Acc)), 
+            n_lane_changes = if(Frame_ID))
+
+vehicle_agg_df <- group_by(individ_vehicles, Vehicle_ID)
+
+arrange(vehicle_agg_df)
+  
